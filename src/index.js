d5e27511d414273f948c2a068f5807eea10f4e05
Karen Kirr <kakirr@microsoft.com>
2019-03-09 22:44:43 -0800

    update states

--- src/index.js
+++ src/index.js
@@ -25,31 +25,72 @@ const style = {
     color: 'grey'
     
 }
-const Book = ({title, author, pages}) => {
+const Book = ({title, author, pages, freeBookmark}) => {
     return(
         <section>
             <h2>{title}</h2>
             <p style={style}>by: {author}</p>
             <p>Pages: {pages} pages</p>
+            <p>Free Bookmark Today: {freeBookmark ? 'yes' : 'no!'}</p>
         </section>
 
     )
 }
-const Library = ({books}) => {
-    return(
-        <div>
-            <h1>Welcome to the Library</h1>
-            {books.map(
-                (book, i) => 
-                <Book 
-                    key={i}
-                    title={book.title} 
-                    author={book.author} 
-                    pages={book.pages}/>
+
+const Hiring = () => 
+    <div>
+        <p>The library is hiring. Go to our website for more info.</p>
+    </div>
+
+const NotHiring = () => {
+    return (
+    <div>
+        <p>The librarry is NOT hiring. Check back at a later date.</p>
+    </div>
+    )
+}
+
+class Library extends Component {
+   
+    state = { 
+        open: false,
+        freeBookmark: false,
+        hiring: true    
+    
+    }
+    
+   
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+
+        }))
+    }
+
+    render () {
+        console.log(this.state)
+        const { books } = this.props
+        return(
+            <div>
+            <h1>Welcome to the Library - the library is {this.state.open ? 'open' : 'closed'}</h1>
+            <p>{this.state.hiring ? <Hiring /> : <NotHiring />}</p>
+            
+            <button onClick = { this.toggleOpenClosed }>Change</button>
+
+                {books.map(
+                    (book, i) => 
+                      <Book 
+                        key={i}
+                        title={book.title} 
+                        author={book.author} 
+                        pages={book.pages}
+                        freeBookmark={this.state.freeBookmark}/>
             )}
         </div>
 
-    )
+         )
+
+    }
 
 }
 
